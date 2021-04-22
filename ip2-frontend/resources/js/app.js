3d48e67587adfff3e1ffbd90ce40cfb8b31b7743
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2021-04-23 00:36:34 +0200

    init - base project (based on demo 09/04)

--- ip2-frontend/resources/js/app.js
+++ ip2-frontend/resources/js/app.js
@@ -1,32 +1,30 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+//Imports
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import routes from './routes.js';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Components global registration
+Vue.component('sidebar', require('./components/partials/SideBar.vue').default);
+Vue.component('event', require('./components/partials/events/Event.vue').default);
+Vue.component('events', require('./components/partials/events/Events.vue').default);
+Vue.component('searchbar', require('./components/partials/SearchBar.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const app = new Vue({
     el: '#app',
+    router: new VueRouter(routes)
 });
