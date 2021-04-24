622c8a260c08b2930206beebf0f07d48c8cb807b
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2021-04-24 19:43:35 +0200

    event: making controller, model, migration

---
+++ ip2-frontend/app/Http/Controllers/EventController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    //
+    public function index()
+    {
+        $events = Event::all()->toArray();
+        return array_reverse($events);
+    }
+
+    public function store(Request $request)
+    {
+        $event = new Event([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'startsAt' => $request->input('startsAt'),
+            'endsAt' => $request->input('endsAt'),
+            'location' => $request->input('location')
+        ]);
+        $event->save();
+
+        return response()->json('Event created!');
+    }
+
+    public function show($id) 
+    {
+        $event = Event::find($id);
+        return response()->json($event);
+    }
+
+    public function update($id, Request $request)
+    {
+        $event = Event::find($id);
+        $event->update($request->all());
+
+        return response()->json('Event updated!');
+    }
+
+    public function destroy($id)
+    {
+        $event = Event::find($id);
+        $event->delete();
+
+        return response()->json('Event deleted');
+    }
+}
