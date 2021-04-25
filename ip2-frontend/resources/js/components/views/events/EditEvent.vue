4f50b6c91e5cd4f15ce9fa3b3b788ec3df546c5f
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2021-04-25 13:32:03 +0200

    design - editEvent form

--- ip2-frontend/resources/js/components/views/events/EditEvent.vue
+++ ip2-frontend/resources/js/components/views/events/EditEvent.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        <h1>Edit Event</h1>
+        <b-form @submit.prevent="editEvent">
+            <b-form-group
+                id="input-group-1"
+                label="Name:"
+                label-for="name">
+
+                <b-form-input
+                id="input-name"
+                v-model="event.name"
+                type="text"
+                placeholder="Enter name..."
+                required>
+                </b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
     </div>
 </template>
 <style scoped>
