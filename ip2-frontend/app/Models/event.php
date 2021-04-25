d55519dd63263ccc041cec16fb13689826f32d4d
Wout Junius <woutyboy555@gmail.com>
2021-04-25 12:21:06 +0200

    SETUP: Setting up the Migrations and Recourse controllers for Event and user.

--- ip2-frontend/app/Models/event.php
+++ ip2-frontend/app/Models/event.php
@@ -5,13 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class user extends Model
+class event extends Model
 {
     use HasFactory;
-
     protected $table = 'events';
 
     public $primaryKey = 'id';
 
     public $timestamps = true;
+    
+    protected $fillable = [
+        'name',
+        'startEvent',
+        'endEvent',
+        'description',
+        'location',
+    ];
 }
--- ip2-frontend/database/migrations/2021_04_24_230505_create_users_table.php
+++ ip2-frontend/database/migrations/2021_04_24_230505_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('id')->primaryKey();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->email('email');
+            $table->date('birthdate');
+            $table->string('role');
+            $table->string('study');
             $table->timestamps();
         });
     }
