9867e3985cb083720657fe2c6659525665afd6e6
SouheibTouri <45950693+SouheibTouri@users.noreply.github.com>
2021-04-25 14:50:40 +0200

    fix - update EditEvent.vue

--- ip2-frontend/resources/js/components/views/events/EditEvent.vue
+++ ip2-frontend/resources/js/components/views/events/EditEvent.vue
@@ -1,22 +1,27 @@
 <template>
     <div>
-        <b-form @submit.prevent="editEvent">
-            <b-form-group
-                id="input-group-1"
-                label="Name:"
-                label-for="name">
+        <div>
+            <h1>Edit event</h1>
+        </div>
+        <div>
+            <b-form @submit.prevent="editEvent">
+                <b-form-group
+                    id="input-group-1"
+                    label="Name:"
+                    label-for="name">
 
-                <b-form-input
-                id="input-name"
-                v-model="event.name"
-                type="text"
-                placeholder="Enter name..."
-                required>
-                </b-form-input>
-            </b-form-group>
+                    <b-form-input
+                    id="input-name"
+                    v-model="event.name"
+                    type="text"
+                    placeholder="Enter name..."
+                    required>
+                    </b-form-input>
+                </b-form-group>
 
-            <b-button type="submit" variant="primary">Submit</b-button>
-        </b-form>
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+        </div>
     </div>
 </template>
 <script>
