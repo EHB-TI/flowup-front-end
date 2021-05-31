71550278e18ace32c2095eb4ffd4fdee4861e155
Tibo De Munck <tibodemunck@gmail.com>
2021-05-31 23:33:58 +0200

    made date bigger

--- ip2-frontend/resources/js/components/views/events/Event.vue
+++ ip2-frontend/resources/js/components/views/events/Event.vue
@@ -231,8 +231,8 @@ export default {
 </script>
 <style scoped>
 .date {
-    width: 80px;
-    height: 80px;
+    width: 100px;
+    height: 100px;
     border-radius: 15px;
     border: solid 1px black;
 }
