b5507af7ca96037d41dabdd15e84d1f56f734677
TiboDeMunck <46536105+TiboDeMunck@users.noreply.github.com>
2021-05-31 13:39:06 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cp /home/frontend/secrets/.env /home/frontend/flowup-front-end/ip2-frontend/.env
+cp /home/frontend/secrets/.env /home/frontend/actions-runner/_work/flowup-front-end/flowup-front-end/ip2-frontend/.env
 cd ip2-frontend/
 composer install
 cd ..
-rm -rf /home/frontend/flowup-front-end/ip2-frontend/vendor/vladimir-yuldashev
-cp -r /home/frontend/vlad/vladimir-yuldashev /home/frontend/flowup-front-end/ip2-frontend/vendor/
-chmod -R +x /home/frontend/flowup-front-end
+rm -rf /home/frontend/actions-runner/_work/flowup-front-end/flowup-front-end/ip2-frontend/vendor/vladimir-yuldashev
+cp -r /home/frontend/vlad/vladimir-yuldashev /home/frontend/actions-runner/_work/flowup-front-end/flowup-front-end/ip2-frontend/vendor/
+chmod -R +x /home/frontend/actions-runner/_work/flowup-front-end/flowup-front-end
 docker-compose build
 docker-compose down
 docker-compose up -d && docker-compose exec app php artisan migrate && docker-compose exec app php artisan key:generate && docker-compose exec app php artisan config:cache
