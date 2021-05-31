01d6c0897ac2d00fc8d4c7b33a30585c6bd094e2
Tibo De Munck <tibodemunck@gmail.com>
2021-05-31 13:30:27 +0200

    deleted no -cache

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ cd ..
 rm -rf /home/frontend/flowup-front-end/ip2-frontend/vendor/vladimir-yuldashev
 cp -r /home/frontend/vlad/vladimir-yuldashev /home/frontend/flowup-front-end/ip2-frontend/vendor/
 chmod -R +x /home/frontend/flowup-front-end
-docker-compose build --no-cache
+docker-compose build
 docker-compose down
 docker-compose up -d && docker-compose exec app php artisan migrate && docker-compose exec app php artisan key:generate && docker-compose exec app php artisan config:cache
