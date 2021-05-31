f0c30feb28b5e46509ab6a1c3b0a7c78988b650c
Tibo De Munck <tibodemunck@gmail.com>
2021-05-31 12:29:51 +0200

    ???

--- startup.sh
+++ startup.sh
@@ -7,4 +7,7 @@ cp -r /home/frontend/vlad/vladimir-yuldashev /home/frontend/flowup-front-end/ip2
 chmod -R +x /home/frontend/flowup-front-end
 docker-compose build
 docker-compose down
-docker-compose up
+docker-compose up -d
+docker-compose exec app php artisan migrate
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan config:cache
