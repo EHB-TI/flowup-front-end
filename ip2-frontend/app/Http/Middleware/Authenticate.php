bcb95f8dda9826aedd1bb342ec0efa3cc0484ab1
Tibo De Munck <tibodemunck@gmail.com>
2021-06-01 18:21:54 +0200

    redirect?

--- ip2-frontend/app/Http/Middleware/Authenticate.php
+++ ip2-frontend/app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('saml2_login', 'AD');
+            return redirect("https://frontend.hypere.be:4433/saml2/AD/login");
         }
     }
 }
