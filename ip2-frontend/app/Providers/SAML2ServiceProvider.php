398763776cca6d8cb789a34c5c2cc47194701fd2
Thekeanu <keanu.piras@student.ehb.be>
2021-06-02 20:59:16 +0200

    fix update backside of email

--- ip2-frontend/app/Providers/SAML2ServiceProvider.php
+++ ip2-frontend/app/Providers/SAML2ServiceProvider.php
@@ -36,7 +36,7 @@ class SAML2ServiceProvider extends ServiceProvider
             $samlUser = $event->getSaml2User();
             $attributes = $samlUser->getAttributes();
             $email_var = $attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"][0];
-            $email = explode("@", $email_var)[0] . "@desideriushogeschool.be";
+            $email = explode("@", $email_var)[0] . "@flowupdesiderius.onmicrosoft.com";
 
             // $laravelUser = //find user by ID or attribute
             //if it does not exist create it and go on  or show an error message
